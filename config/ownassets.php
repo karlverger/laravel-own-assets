e988afaad7eb84d539c48d2d20a410ccce8c7979
karl verger <karl.verger@gmail.com>
2020-11-21 17:15:40 +0100

    add asset properties

--- config/ownassets.php
+++ config/ownassets.php
@@ -13,14 +13,26 @@ return [
      * User tables foreign key name.
      */
     'user_foreign_key' => 'user_id',
+    /*
+     * Asset tables foreign key name.
+     */    
+    'asset_foreign_key'=> 'asset_id',
 
     /*
      * Table name for assets records.
      */
     'assets_table' => 'assets',
+    /*
+     * Table name for assets properties records.
+     */    
+    'assets_properties_table'=>'assets_properties',
 
     /*
      * Model name for asset record.
      */
     'asset_model' => 'Karlverger\LaravelOwnAsset\Asset',
+    /*
+     * Model name for property record.
+     */    
+    'property_model' => 'Karlverger\LaravelOwnAsset\AssetProperty',
 ];
--- src/AssetProperty.php
+++ src/AssetProperty.php
@@ -17,13 +17,13 @@ use Karlverger\LaravelOwnAsset\Events\Asseted;
 use Karlverger\LaravelOwnAsset\Events\Unasseted;
 
 /**
- * Class Asset.
+ * Class AssetProperty.
  *
  * @property \Illuminate\Database\Eloquent\Model $user
  * @property \Illuminate\Database\Eloquent\Model $asseter
  * @property \Illuminate\Database\Eloquent\Model $assetable
  */
-class Asset extends Model
+class AssetProperty extends Model
 {
     /**
      * @var string[]
@@ -35,7 +35,7 @@ class Asset extends Model
 
     public function __construct(array $attributes = [])
     {
-        $this->table = \config('ownassets.assets_table');
+        $this->table = \config('ownassets.assets_properties_table');
 
         parent::__construct($attributes);
     }
@@ -44,47 +44,18 @@ class Asset extends Model
     {
         parent::boot();
 
-        self::saving(function ($asset) {
-            $userForeignKey = \config('ownassets.user_foreign_key');
-            $asset->{$userForeignKey} = $asset->{$userForeignKey} ?: auth()->id();
+        self::saving(function ($assetProperty) {
+            $assetForeignKey = \config('ownassets.asset_foreign_key');
+            $assetProperty->{$assetForeignKey} = $assetProperty->{$assetForeignKey} ;
         });
     }
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
-     */
-    public function assetable()
-    {
-        return $this->morphTo();
-    }
-
-    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function user()
+    public function asset()
     {
-        return $this->belongsTo(\config('auth.providers.users.model'), \config('ownassets.user_foreign_key'));
+        return $this->belongsTo(\config('ownassets.asset_model'), \config('ownassets.asset_foreign_key'));
     }
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function asseter()
-    {
-        return $this->user();
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeWithType(Builder $query, string $type)
-    {
-        return $query->where('assetable_type', app($type)->getMorphClass());
-    }
-
-
-    public function assetProperties()
-    {
-        return $this->hasMany(\config('ownassets.property_model'));
-    }
 }
