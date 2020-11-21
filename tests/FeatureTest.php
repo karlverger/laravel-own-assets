0781c43285dd21f0d1d11e0b1f1f2d4e1ed3fe27
karl verger <karl.verger@gmail.com>
2020-11-21 18:15:41 +0100

    add asset properties testing

--- tests/FeatureTest.php
+++ tests/FeatureTest.php
@@ -37,7 +37,7 @@ class FeatureTest extends TestCase
 
         $user->asset($post);
 
-        $myAsset = $user->assets()->get();
+        $myAsset = $user->assets()->where("assetable_type",Post::class)->get();
         //$myAsset->addAssetProperty("clef","value");
 
         Event::assertDispatched(Asseted::class, function ($event) use ($user, $post) {
